ce2dee53893fa62860a196a084f865d819d9e889
amliebeq <alex.lieberman182@gmail.com>
2024-01-25 16:11:29 -0700

    updated proxy and tests for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(0)
+  // const [user, setUser] = useState('')
+
+  // useEffect (() => {
+  //   fetch('/api/users/1') .then((r) => {if (r.ok) {r.json().then ((user) => setUser(user))}})
+  // },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount2((count) => count + 1)}>
-          count is {count2}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* {user.first_name} {user.last_name} */}
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // the target server for API requests
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }

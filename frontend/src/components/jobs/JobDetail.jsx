9ae5e5b14eed3b66148990ff846945f837f6ba7d
amliebeq <alex.lieberman182@gmail.com>
2024-03-07 11:26:47 -0700

    updated job details

--- frontend/src/components/jobs/JobDetail.jsx
+++ frontend/src/components/jobs/JobDetail.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 export const JobDetail = () => {
     const { id } = useParams()
-    const jobs = useSelector(state => state.user.user.jobs)
-    const job = jobs.find(job => job.id == id)
-    console.log(job)
+    const user = useSelector(state => state.user.user)
+    const job = user.jobs.find(job => job.id == id)
+    const job_applicants = user.applicants.filter(applicant => applicant.job_id == id)
+    
   return (
-    <div>JobDetail</div>
+    <div>
+      <h1>{job.title}</h1>
+      <p>{job.description}</p>
+      <p>{job.location}</p>
+      {job.remote === true ? <p>Remote</p> : <p>In person</p>}
+    </div>
   )
 }
